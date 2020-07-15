571e6e2489d07174ad7c4e01b987e3a0d51740fb
SezerK <skartal9@gmail.com>
2020-07-15 12:35:22 +0300

    sales-basket component created.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React, { Component } from 'react';
 import './App.scss';
 import Filter from './components/filters/filters';
 import Products from "./components/products/products";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import fontawesome from '@fortawesome/fontawesome';
-import { faShoppingCart } from '@fortawesome/fontawesome-free-solid';
-
-fontawesome.library.add(faShoppingCart);
+import { SalesBasket } from "./components/sales-basket/sales-basket";
 
 class App extends Component{
 
@@ -31,89 +27,7 @@ class App extends Component{
                     <Products filteredSizes={this.state.filteredSizes}></Products>
                 </main>
                 <nav>
-                    <div className="close">
-                        <span>X</span>
-                    </div>
-                    <div className="shopping-card">
-                        <header>
-                            <div>
-                                <FontAwesomeIcon icon={"shopping-cart"} size={"2x"}/>
-                                <span className="dot">5</span>
-                            </div>
-                            <span>Cart</span>
-                        </header>
-                        <section className="product-row">
-                            <div className="product-row">
-                                <div className="col-1">
-                                    <img src={require(`./static/products/100_2.jpg`)} alt={""} />
-                                </div>
-                                <div className="col-2">
-                                    col2
-                                </div>
-                                <div className="col-3">
-                                    col3
-                                </div>
-                            </div>
-
-                            <div className="product-row">
-                                <div className="col-1">
-                                    <img src={require(`./static/products/100_2.jpg`)} alt={""} />
-                                </div>
-                                <div className="col-2">
-                                    col2
-                                </div>
-                                <div className="col-3">
-                                    col3
-                                </div>
-                            </div>
-
-                            <div className="product-row">
-                                <div className="col-1">
-                                    <img src={require(`./static/products/100_2.jpg`)} alt={""} />
-                                </div>
-                                <div className="col-2">
-                                    col2
-                                </div>
-                                <div className="col-3">
-                                    col3
-                                </div>
-                            </div>
-
-                            <div className="product-row">
-                                <div className="col-1">
-                                    <img src={require(`./static/products/100_2.jpg`)} alt={""} />
-                                </div>
-                                <div className="col-2">
-                                    col2
-                                </div>
-                                <div className="col-3">
-                                    col3
-                                </div>
-                            </div>
-
-                            <div className="product-row">
-                                <div className="col-1">
-                                    <img src={require(`./static/products/100_2.jpg`)} alt={""} />
-                                </div>
-                                <div className="col-2">
-                                    col2
-                                </div>
-                                <div className="col-3">
-                                    col3
-                                </div>
-                            </div>
-                        </section>
-                        <footer>
-                            <div className="sub">
-                                <span className="sub-total">SUBTOTAL</span>
-                                <div className="sub-price">
-                                    <span>$ 19.90</span>
-                                    <small>OR UP TO 9 x $ 2.21</small>
-                                </div>
-                            </div>
-                            <button>CHECKOUT</button>
-                        </footer>
-                    </div>
+                    <SalesBasket></SalesBasket>
                 </nav>
             </div>
     );
---
+++ src/components/sales-basket/sales-basket.js
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './sales-basket.scss';
+import fontawesome from '@fortawesome/fontawesome';
+import { faShoppingCart } from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add(faShoppingCart);
+
+export class SalesBasket extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <div>
+                <div className="close">
+                    <span>X</span>
+                </div>
+                <div className="shopping-card">
+                    <header>
+                        <div>
+                            <FontAwesomeIcon icon={"shopping-cart"} size={"2x"}/>
+                            <span className="dot">5</span>
+                        </div>
+                        <span>Cart</span>
+                    </header>
+                    <section className="product-row">
+                        <div className="product-row">
+                            <div className="col-1">
+                                <img src={require(`../../static/products/100_2.jpg`)} alt={""} />
+                            </div>
+                            <div className="col-2">
+                                col2
+                            </div>
+                            <div className="col-3">
+                                col3
+                            </div>
+                        </div>
+
+                        <div className="product-row">
+                            <div className="col-1">
+                                <img src={require(`../../static/products/100_2.jpg`)} alt={""} />
+                            </div>
+                            <div className="col-2">
+                                col2
+                            </div>
+                            <div className="col-3">
+                                col3
+                            </div>
+                        </div>
+
+                        <div className="product-row">
+                            <div className="col-1">
+                                <img src={require(`../../static/products/100_2.jpg`)} alt={""} />
+                            </div>
+                            <div className="col-2">
+                                col2
+                            </div>
+                            <div className="col-3">
+                                col3
+                            </div>
+                        </div>
+
+                        <div className="product-row">
+                            <div className="col-1">
+                                <img src={require(`../../static/products/100_2.jpg`)} alt={""} />
+                            </div>
+                            <div className="col-2">
+                                col2
+                            </div>
+                            <div className="col-3">
+                                col3
+                            </div>
+                        </div>
+
+                        <div className="product-row">
+                            <div className="col-1">
+                                <img src={require(`../../static/products/100_2.jpg`)} alt={""} />
+                            </div>
+                            <div className="col-2">
+                                col2
+                            </div>
+                            <div className="col-3">
+                                col3
+                            </div>
+                        </div>
+                    </section>
+                    <footer>
+                        <div className="sub">
+                            <span className="sub-total">SUBTOTAL</span>
+                            <div className="sub-price">
+                                <span>$ 19.90</span>
+                                <small>OR UP TO 9 x $ 2.21</small>
+                            </div>
+                        </div>
+                        <button>CHECKOUT</button>
+                    </footer>
+                </div>
+            </div>
+    );
+    }
+}
